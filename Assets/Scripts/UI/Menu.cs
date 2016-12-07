3d81b18925824c95e6c0e68eb446461e4b580d8d
Nigel Martinez <nigel-martinez@hotmail.com>
2016-12-06 20:28:15 -0500

    Removed pause menu and touched up chat.

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -4,6 +4,8 @@
  * Description: Handles scene transitions and the in-game pause menu.
  */
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
@@ -21,10 +23,23 @@ public class Menu : MonoBehaviour {
         }
     }
 
+    //Highlight first option for controller-friendly menu.
+    public IEnumerator InitSelect() {
+        if (pauseMenu) {
+            GameObject panel = GameObject.Find("Pause Menu").gameObject;
+            Button host = panel.transform.Find("Menu Button").gameObject.GetComponent<Button>();
+
+            EventSystem.current.SetSelectedGameObject(null);
+            yield return null;
+            EventSystem.current.SetSelectedGameObject(host.gameObject);
+        }
+    }
+
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Escape) && pauseMenu) {
             if (!active) {
+                StartCoroutine("InitSelect");
                 active = true;
                 menu.alpha = 1;
                 menu.interactable = true;
@@ -41,7 +56,17 @@ public class Menu : MonoBehaviour {
         SceneManager.LoadSceneAsync(scene, mode);
     }
 
+    public void BackToMenu() {
+        //TODO: Write in disconnect shit.
+
+
+        LoadSceneMode mode = LoadSceneMode.Single;
+        SceneManager.LoadSceneAsync("Pregame Menu", mode);
+    }
+
     public void ExitGame() {
+        //TODO: Write disconnect shit.
+
         Application.Quit();
     }
 }
